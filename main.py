01c7f06229de90fe79f10ec1b773e7fbf5909cac
Aswin Kumar <achu4798@gmail.com>
2024-04-25 21:32:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def create_block_header(merkle_root, timestamp, previous_block_hash, nonce):
         "previous_block_hash": previous_block_hash,
         "merkle_root": merkle_root,
         "timestamp": timestamp,
-        "bits": "1f00ffff", 
+        "bits": "1d00ffff", 
         "nonce": nonce
     }
 
